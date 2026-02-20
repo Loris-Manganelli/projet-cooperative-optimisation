ec1efefe64e079f3d428f1db6631020ebe6f38e4
NoeCasteleyn <noe.casteleyn@polytechnique.edu>
2026-02-20 19:26:32 +0100

    Dual_Decomposition

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Centralized_solution import solve, plot_me, Cov2, Cov
 import numpy as np
 from DGD import DGD
 from GT import GT
+from Dual_decomposition import dual_decomposition
 from visualisation import make_gap_graph
 import matplotlib.pyplot as plt
 with open('data/first_database.pkl', 'rb') as f:
@@ -11,10 +12,17 @@ with open('data/first_database.pkl', 'rb') as f:
 
 ### PARAMETERS TO BE MODIFIED
 num_points = 100
-n_iter = 20000
+n_iter = 50000
 step_size = 0.001
+
+# Graph topology :
 a = 5 # number of agents
 W = np.ones([a,a])/a # consensus matrix (fully connected graph) TO BE MODIFIED for other topologies
+#adjacency matrix of the graph (circular graph)
+A = np.zeros([a,a])
+for i in range(a):
+    A[i,(i+1)%a] = 1
+    A[(i+1)%a,i] = 1
 
 ## DATA PREPARATION
 x = x[:num_points]
@@ -30,15 +38,27 @@ indices = np.random.permutation(n) # random permutation of the data points to be
 K_a = [K_nm[indices[i*points_per_agent:(i+1)*points_per_agent], :] for i in range(a)]
 y_a = [y[indices[i*points_per_agent:(i+1)*points_per_agent]] for i in range(a)]
 
-## DGD SOLVE 
+
 alpha_0 = np.zeros((a,m)) # Initialization of the local variables for each agent
+multipliers_0 = np.zeros((a*m,)) # Initialization of the multipliers for dual decomposition
+
+## DGD SOLVE 
 alpha_dgd = DGD(alpha_0, K_a, K_mm, y_a, W, sigma=0.5, nu=1.0, max_iter=n_iter, lr=step_size)
+## GT SOLVE
 alpha_gt = GT(alpha_0, K_a, K_mm, y_a, W, sigma=0.5, nu=1.0, max_iter=n_iter, lr=step_size)
+## DUAL DECOMPOSITION SOLVE
+alpha_dualdecomp, multipliers = dual_decomposition(multipliers_0, K_a, K_mm, y_a, A, sigma=0.5, nu=1.0, max_iter=n_iter, lr=10*step_size)   
+
+
+# test de forat : 
+# print ("alpha_dgd shape : ", alpha_dgd[-1].shape)
+# print ("alpha_gt shape : ", alpha_gt[-1].shape)
+# print ("alpha_dualdecomp shape : ", alpha_dualdecomp[-1].shape)
 
 # PLOTS
 plot_me(x[:num_points],y[:num_points], alpha, ind, selection=True)
-
-alphaDict = {'DGD': alpha_dgd, 'GT': alpha_gt}
+   
+alphaDict = {'DGD': alpha_dgd, 'GT': alpha_gt, 'Dual Decomposition': alpha_dualdecomp}
 
 make_gap_graph(alpha, alphaDict)
 
